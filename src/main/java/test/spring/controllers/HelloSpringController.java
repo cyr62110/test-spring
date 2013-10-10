b972a7d5b99ebd80e869309d8fd8b37ec9a97c18
Cyril Vlaminck <developer@cvlaminck.fr>
2013-10-10 15:53:06 +0200

    Working HelloSpring with Controller

--- src/main/java/test/spring/controllers/HelloSpringController.java
+++ src/main/java/test/spring/controllers/HelloSpringController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class HelloSpringController {
 
     @RequestMapping(method = RequestMethod.GET)
