3dffce5115fbf2fea848ed1b83ba3554f971961f
Cyril Vlaminck <developer@cvlaminck.fr>
2013-10-11 01:18:05 +0200

    Working a bit on Thymeleaf
    Created a new controller for the book
    Modified web.xml to support css and js
    Removed old index.html

--- src/main/java/test/spring/controllers/GoldenBookController.java
+++ src/main/java/test/spring/controllers/GoldenBookController.java
@@ -2,26 +2,29 @@ package test.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import test.spring.entities.Message;
 import test.spring.repositories.MessageRepository;
 
 @Controller
 @RequestMapping("/")
-public class HelloSpringController {
+public class GoldenBookController {
 
     @Autowired
-    MessageRepository messageRepository;
+    private MessageRepository messageRepository;
+
+    private Message message = new Message();
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public String helloSpring()
+    public String getGoldenBook()
     {
-        Message m = new Message("Mon message");
-        messageRepository.save(m);
-        return m.toString();
+        return "/goldenBook";
     }
 
+    @ModelAttribute("message")
+    public Message getMessage() {
+        return message;
+    }
 }
